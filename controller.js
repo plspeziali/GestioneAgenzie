b449033a736565802acc06e03654166e2ff3e97e
Paolo Speziali <speziali99@gmail.com>
2018-03-14 17:24:28 +0100

    Aggiunta di metodi essenziali

--- controller.js
+++ controller.js
@@ -7,9 +7,12 @@ function sendAg() {
 }
 
 function sendCl() {
-    var name = $("#nameAg").val();
-    $("#nameAg").val("");
-    $.post("sendAg.php?name="+name, function(data, status){
-        $('#divAlert').append("<div class='alert alert-info alert-dismissible fade in'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>Agente "+name+" correttamente aggiunto al database!</div>");
+    var rag = $("#ragCl").val();
+    var reg = $("#regCl").val();
+    var agent = $("#agentCl").val();
+    $("#ragCl").val("");
+    $("#regCl").val("");
+    $.post("sendCl.php?rag="+rag+"&reg="+reg+"&agent="+agent, function(data, status){
+        $('#divAlert').append("<div class='alert alert-info alert-dismissible fade in'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>Cliente "+rag+" correttamente aggiunto al database!</div>");
     });
 }
